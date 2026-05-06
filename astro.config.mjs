73ba537e5c375f2b30372b59003c62210e742cc9
liurenqin <liurenqin@bytedance.com>
2026-05-06 20:22:49 +0800

    refactor: extract views for i18n pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "zh"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+	en: 'EN',
+	zh: '中',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+	en: {
+		'nav.profile': 'Profile',
+		'nav.blog': 'Blog',
+		'nav.links': 'Links',
+		'nav.milestone': 'Milestone',
+		'site.title': 'CodeCyrus - Think twice, code once.',
+		'site.subtitle': 'Think twice, code once.',
+	},
+	zh: {
+		'nav.profile': '个人主页',
+		'nav.blog': '博客文章',
+		'nav.links': '朋友们',
+		'nav.milestone': '大事记',
+		'site.title': 'CodeCyrus - Think twice, code once.',
+		'site.subtitle': 'Think twice, code once.',
+	},
+} as const;
